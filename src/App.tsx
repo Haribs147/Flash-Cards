d5f5f315dbbda087e9a403bcb8c95c07c2d3cb7e
Michal <m.jagodzinski1234@gmail.com>
2025-06-29 17:43:15 +0200

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './App.css'
 import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
+
+  const [activePage, setActivePage] = useState('home');
 
   return (
     
     <div className='App'>
       <Navbar />
+      <Sidebar activeItem={activePage} onItemClick={setActivePage} />
+        
+      <div className='main-content'>
+        essa
+      </div>
     </div>
 
   )
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { FiMenu, FiSearch } from 'react-icons/fi'; // Importujemy ikony
-import './Navbar.css'; // Importujemy dedykowany plik CSS
+import { FiMenu } from 'react-icons/fi';
+import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      {/* --- Sekcja lewa --- */}
+
       <div className="navbar-left">
         <button className="navbar-button">
           <FiMenu size={24} />
@@ -14,14 +13,13 @@ const Navbar = () => {
       </div>
 
       {/* --- Sekcja środkowa (wyszukiwarka) --- */}
-      <div className="navbar-center">
+      {/* <div className="navbar-center">
         <div className="search-container">
           <FiSearch className="search-icon" size={20} />
           <input type="text" placeholder="Szukaj" className="search-input" />
         </div>
-      </div>
+      </div> */}
 
-      {/* --- Sekcja prawa (awatar) --- */}
       <div className="navbar-right">
         <div className="user-avatar">M</div>
       </div>
