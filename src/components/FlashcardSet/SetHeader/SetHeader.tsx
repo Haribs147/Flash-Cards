6d5dfee70c34363f23c91da122e49bd7b67cc35d
Michal <m.jagodzinski1234@gmail.com>
2025-07-12 19:56:23 +0200

    prettier formating

--- src/components/FlashcardSet/SetHeader/SetHeader.tsx
+++ src/components/FlashcardSet/SetHeader/SetHeader.tsx
@@ -1,30 +1,42 @@
-import { FiEdit, FiShare2, FiX, FiCopy } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
-import './SetHeader.css';
+import { FiEdit, FiShare2, FiX, FiCopy } from "react-icons/fi";
+import "./SetHeader.css";
 
 type SetHeaderProps = {
-    title: string;
-    description: string;
-    initial: string;
-    onBackClick: () => void;
+  title: string;
+  description: string;
+  initial: string;
+  onBackClick: () => void;
 };
 
-const SetHeader = ({ title, description, initial, onBackClick  }: SetHeaderProps) => {
-    return (
-        <header className="set-header">
-            <div className="set-avatar">{initial}</div>
-            <div className="set-info">
-                <h1 className="set-title">{title}</h1>
-                <p className="set-description">{description}</p>
-            </div>
-            <div className="set-header-actions">
-                <button className="icon-btn"><FiEdit/></button>
-                <button className="icon-btn"><FiCopy/></button>
-                <button className="icon-btn"><FiShare2/></button>
-                <button  onClick={onBackClick} className="icon-btn close-link"><FiX/></button >
-            </div>
-        </header>
-    );
+const SetHeader = ({
+  title,
+  description,
+  initial,
+  onBackClick,
+}: SetHeaderProps) => {
+  return (
+    <header className="set-header">
+      <div className="set-avatar">{initial}</div>
+      <div className="set-info">
+        <h1 className="set-title">{title}</h1>
+        <p className="set-description">{description}</p>
+      </div>
+      <div className="set-header-actions">
+        <button className="icon-btn">
+          <FiEdit />
+        </button>
+        <button className="icon-btn">
+          <FiCopy />
+        </button>
+        <button className="icon-btn">
+          <FiShare2 />
+        </button>
+        <button onClick={onBackClick} className="icon-btn close-link">
+          <FiX />
+        </button>
+      </div>
+    </header>
+  );
 };
 
 export default SetHeader;
