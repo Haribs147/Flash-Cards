93869509ce9cd8bd8695a3d6927ed1ebe7c0dd93
Michal <m.jagodzinski1234@gmail.com>
2025-07-20 17:14:04 +0200

    removed comments

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,5 +1,5 @@
-import { configureStore } from '@reduxjs/toolkit';
-import materialsReducer from '../features/materials/materialsSlice.tsx';
+import { configureStore } from "@reduxjs/toolkit";
+import materialsReducer from "../features/materials/materialsSlice.tsx";
 
 export const store = configureStore({
   reducer: {
@@ -7,6 +7,5 @@ export const store = configureStore({
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
