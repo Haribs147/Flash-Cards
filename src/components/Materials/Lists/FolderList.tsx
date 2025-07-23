9521b5a716945bd62c3d5ba0e5968b63657af174
Michal <m.jagodzinski1234@gmail.com>
2025-07-23 19:15:57 +0200

    moving folder down the breadcrumbs

--- src/components/Materials/Lists/FolderList.tsx
+++ src/components/Materials/Lists/FolderList.tsx
@@ -3,15 +3,78 @@ import { useNavigate } from "react-router-dom";
 import "./FolderList.css";
 import NewFolderInput from "../CreateFolderButton/CreateFolder";
 import {
+    moveItem,
     setCurrentFolderId,
     type MaterialItem,
 } from "../../../features/materials/materialsSlice";
 import { useAppDispatch } from "../../../app/hooks";
+import { useState } from "react";
 
 type FolderListProps = {
     filteredItems: MaterialItem[];
     isCreating: boolean;
     onCreate: (name: string) => void;
+};
+
+const FolderListItem = ({
+    item,
+    onClick,
+    onDragStart,
+}: {
+    item: MaterialItem;
+    onClick: (item: MaterialItem) => void;
+    onDragStart: (e: React.DragEvent<HTMLDivElement>, itemId: string) => void;
+}) => {
+    const [isOver, setIsOver] = useState(false);
+    const dispatch = useAppDispatch();
+
+    const handleDragDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setIsOver(false);
+        const draggedItemId = e.dataTransfer.getData("text/plain");
+        if (item.type === "folder" && item.id != draggedItemId) {
+            dispatch(
+                moveItem({ itemId: draggedItemId, targetFolderId: item.id }),
+            );
+        }
+    };
+
+    const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        const draggedItemId = e.dataTransfer.getData("text/plain");
+        if (item.type === "folder" && item.id != draggedItemId) {
+            console.log(
+                "item.id = " + item.id + "draggedItemId = " + draggedItemId,
+            );
+            setIsOver(true);
+        }
+    };
+
+    const handleDragLeave = () => {
+        setIsOver(false);
+    };
+
+    return (
+        <div
+            draggable={true}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDragDrop}
+            onDragStart={(e) => onDragStart(e, item.id)}
+            key={item.id}
+            className={`list-item ${isOver ? "drop-target" : ""}`}
+            onClick={() => onClick(item)}
+        >
+            <div className="item-icon">
+                {item.type === "folder" ? (
+                    <FiFolder size={22} />
+                ) : (
+                    <FiFileText size={22} />
+                )}
+            </div>
+            <span className="item-name">{item.name}</span>
+        </div>
+    );
 };
 
 const FolderList = ({
@@ -42,22 +105,11 @@ const FolderList = ({
             {isCreating && <NewFolderInput onCreate={onCreate} />}
 
             {filteredItems.map((item) => (
-                <div
-                    draggable={true}
-                    onDragStart={(e) => handleDragStart(e, item.id)}
-                    key={item.id}
-                    className="list-item"
-                    onClick={() => handleItemClick(item)}
-                >
-                    <div className="item-icon">
-                        {item.type === "folder" ? (
-                            <FiFolder size={22} />
-                        ) : (
-                            <FiFileText size={22} />
-                        )}
-                    </div>
-                    <span className="item-name">{item.name}</span>
-                </div>
+                <FolderListItem
+                    item={item}
+                    onClick={handleItemClick}
+                    onDragStart={handleDragStart}
+                />
             ))}
         </div>
     );
