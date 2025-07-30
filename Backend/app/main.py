d4fab0b50d11e8471a324abe8bdd052815b8e5a7
Michal <m.jagodzinski1234@gmail.com>
2025-07-30 08:04:17 +0200

    password min length and req

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,5 +1,7 @@
+from typing import Annotated
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel, EmailStr
 from sqlalchemy.orm import Session
 
@@ -19,24 +21,42 @@ class Token(BaseModel):
     access_token: str
     token_type: str
 
+def is_password_strong(password: str):
+    special_characters = "!@#$%^&*()-+?_=,<>/"
+
+    if len(password) < 8:
+        return False
+    if not any (char.isdigit() for char in password):
+        return False
+    if not any (char.isupper() for char in password):
+        return False
+    if not any (char.islower() for char in password):
+        return False
+    if not any (char in special_characters for char in password):
+        return False
 
 @app.post("/register", status_code=status.HTTP_201_CREATED)
 def register_user(user_in: UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user_in.email).first()
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    
+    if is_password_strong(user_in.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Password is not stron enough, it must be at least 8 characters long and contain: upper letter, lower letter, number and special character")
+
     hashed_password = get_password_hash(user_in.password)
     new_user = User(email=user_in.email, password=hashed_password)
+
     db.add(new_user)
     db.commit()
     return {"message": "User registered successfully"}
 
 @app.post("/login", response_model=Token)
-def login_for_access_token(user_in: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user_in.email).first()
-    if not db_user or not verify_password(user_in.password, db_user.password):
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == form_data.email).first()
+
+    if not db_user or not verify_password(form_data.password, db_user.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorecct email or password")
+    
     access_token = create_acces_token(data={"sub": db_user.email})
     return{"access_token": access_token, "token_type": "bearer"}
 
